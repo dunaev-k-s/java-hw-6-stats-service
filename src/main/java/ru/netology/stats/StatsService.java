053c3ac9179d587a8da725257b37ec9b822d84a6
Konstantin Dunaev <shop.overled@gmail.com>
2021-04-12 16:05:14 +0300

    Написал методы вычислений

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -3,6 +3,7 @@ package ru.netology.stats;
 public class StatsService {
     int[] monthlySales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
     int arrayLength = monthlySales.length;
+    int average = 0;
 
     public int calculateSumForSales(int[] monthlySales) {
         int sum = 0;
@@ -26,12 +27,46 @@ public class StatsService {
         int number = 0;
         int currentMax = monthlySales[0];
         for (int sale : monthlySales) {
-            number ++;
-            if (currentMax < sale){
+            number++;
+            if (currentMax < sale) {
                 currentMax = sale;
                 number = numberMax;
             }
         }
         return numberMax;
+    }
+
+    public int calculateNumberLastMinSale(int[] monthlySales) {
+        int numberMin = 0;
+        int number = 0;
+        int currentMin = monthlySales[0];
+        for (int sale : monthlySales) {
+            number++;
+            if (currentMin > sale) {
+                currentMin = sale;
+                number = numberMin;
+            }
+        }
+        return numberMin;
+    }
+
+    public int calculateQuantityBelowAverage(int[] monthlySales) {
+        int numberBelowAverage = 0;
+        for (int sale : monthlySales) {
+            if (average > sale) {
+                numberBelowAverage++;
+            }
+        }
+        return numberBelowAverage;
+    }
+
+    public int calculateQuantityAboveAverage(int[] monthlySales) {
+        int numberAboveAverage = 0;
+        for (int sale : monthlySales) {
+            if (average < sale) {
+                numberAboveAverage++;
+            }
+        }
+        return numberAboveAverage;
     }
 }
